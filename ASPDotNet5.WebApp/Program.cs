de4d2dd86b1416a128208c07544dbbbe508833cf
Narayanan Sankaranarayanan <Narayanan.Sankaranarayanan@toyota.com>
2016-06-08 13:57:22 -0700

    20160608 - Test02

--- ASPDotNet5.WebApp/Program.cs
+++ ASPDotNet5.WebApp/Program.cs
@@ -13,7 +13,7 @@ namespace ASPDotNet5.WebApp
         {
             var host = new WebHostBuilder()
                 //.UseUrls(Environment.GetEnvironmentVariable("ASPNETCORE_SERVER.URLS") ?? String.Empty)
-                .UseUrls("http://0.0.0.0:5001", "http://0.0.0.0:5002", "http://0.0.0.0:5003", "http://0.0.0.0:5004", "http://0.0.0.0:5005")
+                .UseUrls("http://0.0.0.0:80", "http://0.0.0.0:5001", "http://0.0.0.0:5002", "http://0.0.0.0:5003", "http://0.0.0.0:5004", "http://0.0.0.0:5005")
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()

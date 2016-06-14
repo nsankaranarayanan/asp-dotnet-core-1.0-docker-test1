e60fba36faa6932f7ad4a32ecdca8b966a3915b3
Narayanan Sankaranarayanan <Narayanan.Sankaranarayanan@toyota.com>
2016-06-14 13:08:17 -0700

    20160614 - Test01. Added two API controllers.

---
+++ ASPDotNet5.WebApp/Controllers/MiscellaneousController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPDotNet5.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class MiscellaneousController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public string Get()
+        {
+            return ("Hello World - " + DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss tt"));
+        }
+    }
+}
